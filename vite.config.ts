5096f8c70faf4fa54d2d3ad72e49a07ff748e97c
elvisleyva49 <elvisleyva49@gmail.com>
2026-05-21 05:29:54 -0500

    Primer despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CATEC2026/', // <-- IMPORTANTE: Si tu repo en GitHub tiene otro nombre, cambialo aqui
 })
